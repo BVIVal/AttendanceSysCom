d7d99904b5a05244b67f875aa79c28ec878df48a
Valentin Begunov <bvival@gmail.com>
2020-05-04 14:48:59 +1000

    Added different haarcscade-algorithms;

---
+++ CameraCapture/Algorithms/Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace CameraCapture.Algorithms
+{
+    public class Settings
+    {
+        public double ScaleRate { get; set; } = 1.1;
+        public int MinNeighbors { get; set; } = 2;
+        public int MinWindowSize { get; set; } = 25;
+    }
+}
