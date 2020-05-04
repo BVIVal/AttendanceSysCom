362a8f7019d987d55068d102f979c0bcf49ef001
Valentin Begunov <bvival@gmail.com>
2020-05-04 14:49:52 +1000

    Added String- & Invoke-extensions.

---
+++ CameraCapture/Extensions/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using System.Linq;
+
+namespace CameraCapture.Extensions
+{
+    public static class StringExtensions
+    {
+        public static double ToDouble(this string text, double defaultValue = default)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return defaultValue;
+            }
+
+            var normalizedText = new string(text.Where(c => char.IsDigit(c) || c == '.' || c == ',' || c == '-').ToArray());
+            const NumberStyles styles = NumberStyles.AllowLeadingWhite |
+                                        NumberStyles.AllowTrailingWhite |
+                                        NumberStyles.AllowDecimalPoint |
+                                        NumberStyles.AllowLeadingSign;
+
+            return double.TryParse(normalizedText.Replace(',', '.'), styles, CultureInfo.InvariantCulture, out var result)
+                ? result : defaultValue;
+        }
+
+        public static int ToInteger(this string text, int defaultValue = default)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return defaultValue;
+            }
+
+            return int.TryParse(text, out var result)
+                ? result
+                : defaultValue;
+        }
+    }
+}
---
+++ CameraCapture/Extensions/SynchronizeInvokeExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CameraCapture.Extensions
+{
+    public static class SynchronizeInvokeExtensions
+    {
+        public static void InvokeIfRequired<T>(this T value, Action<T> action)
+            where T : ISynchronizeInvoke
+        {
+            try
+            {
+                if (value.InvokeRequired)
+                {
+                    value.Invoke(action, new object[] { value });
+                }
+                else
+                {
+                    action(value);
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                // ignored
+            }
+        }
+
+        public static TOut InvokeIfRequired<TIn, TOut>(this TIn value, Func<TIn, TOut> func)
+            where TIn : ISynchronizeInvoke
+        {
+            try
+            {
+                return value.InvokeRequired
+                    ? (TOut)value.Invoke(func, new object[] { value })
+                    : func(value);
+            }
+            catch (ObjectDisposedException)
+            {
+                return default;
+            }
+        }
+    }
+}
