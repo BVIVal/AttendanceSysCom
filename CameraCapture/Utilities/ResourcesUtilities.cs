ebd754d1b0fc31d8a3e5125c6a3df3936f0d3c7f
Valentin Begunov <bvival@gmail.com>
2020-06-01 13:33:30 +1000

    Added GetResourceBytes

--- CameraCapture/Utilities/ResourcesUtilities.cs
+++ CameraCapture/Utilities/ResourcesUtilities.cs
@@ -18,5 +18,18 @@ namespace CameraCapture.Utilities
                 return reader.ReadToEnd();
             }
         }
+
+        public static byte[] GetResourceBytes(string name, Assembly assembly = null)
+        {
+            assembly = assembly ?? Assembly.GetExecutingAssembly();
+            var resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith(name));
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName) ?? throw new ArgumentException($"\"{name}\" is not found in embedded resources"))
+            {
+                var bytes = new byte[stream.Length];
+                stream.Read(bytes, 0, bytes.Length);
+                return bytes;
+            }
+        }
     }
 }
