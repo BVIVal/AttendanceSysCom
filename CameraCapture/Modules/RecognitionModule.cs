8d492480b3f06425dcacd0a32968cac5413cbb92
Valentin Begunov <bvival@gmail.com>
2020-06-02 11:45:39 +1000

    Recognition module: added ctor and main functions;

--- CameraCapture/Modules/RecognitionModule.cs
+++ CameraCapture/Modules/RecognitionModule.cs
@@ -1,7 +1,48 @@
-﻿namespace CameraCapture.Modules
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Emgu.CV;
+using Emgu.CV.Face;
+
+namespace CameraCapture.Modules
 {
     public class RecognitionModule
     {
-        
+        private FaceRecognizer Recognizer { get; set; }
+
+        public RecognitionModule(RecognitionModuleEnum chosenMethod)
+        {
+            switch (chosenMethod)
+            {
+                case RecognitionModuleEnum.EigenFaceRecognizer:
+                    Recognizer = new EigenFaceRecognizer();
+                    break;
+                case RecognitionModuleEnum.FisherFaceRecognizer:
+                    Recognizer = new FisherFaceRecognizer();
+                    break;
+                case RecognitionModuleEnum.LbphFaceRecognizer:
+                    Recognizer = new LBPHFaceRecognizer();
+                    break;
+                case RecognitionModuleEnum.OpenFaceRecognizer:
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(chosenMethod), chosenMethod, null);
+            }
+        }
+
+        public void Train(IList<(int, Mat, DateTime)> labeledFaces, string path)
+        {
+            Recognizer.Train(labeledFaces.Select(f => f.Item2).ToArray(), labeledFaces.Select(f => f.Item1).ToArray());
+            Recognizer.Write(path);
+        }
+
+        public FaceRecognizer.PredictionResult Predict(Mat image)
+        {
+            return Recognizer.Predict(image);
+        }
+
+        public void Load(string path)
+        {
+            Recognizer.Read(path);
+        }
     }
 }
---
+++ CameraCapture/Modules/RecognitionModuleEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace CameraCapture.Modules
+{
+    public enum RecognitionModuleEnum
+    {
+        EigenFaceRecognizer = 1,
+        FisherFaceRecognizer = 2,
+        LbphFaceRecognizer = 3,
+        OpenFaceRecognizer = 4
+
+    }
+}
