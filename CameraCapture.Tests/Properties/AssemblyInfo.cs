e19ccd3eb0c49e2625db658254859985eef51ade
Valentin Begunov <bvival@gmail.com>
2020-06-02 11:47:48 +1000

    Added TestProject;

---
+++ CameraCapture.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CameraCapture.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CameraCapture.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ccf87908-2745-4eaa-847d-1a744894b68f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CameraCapture.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CameraCapture.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //if (!CameraImage.TryCascadeRecognition(originalFrames, CascadeClassifierList[0], Settings, Color.Blue))
+            //    MessageBox.Show(@"TryCascadeRecognition 0- false");
+            //if (!CameraImage.TryCascadeRecognition(CameraImage.ResultFrame.Mat, CascadeClassifierList[1], Settings, Color.Red))
+            //    MessageBox.Show(@"TryCascadeRecognition 1- false");
+        }
+    }
+}
